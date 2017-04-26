22af1ee3f38b8f3babf9349460262ed425344fa9
Joe Gouldie <JosephGouldie@Hotmail.com>
2017-04-26 15:51:31 +0100

    Extracting code that isnt used

--- Javascript Project/js/WowSliderScript.js
+++ Javascript Project/js/WowSliderScript.js
@@ -1,1 +1,1 @@
-jQuery("#wowslider-container").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:20*100,width:580,height:212,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
+jQuery("#wowslider-container").wowSlider({effect:"rotate",prev:"",next:"",duration:80*100,delay:20*100,width:800,height:400,autoPlay:true,stopOnHover:true,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:3});
